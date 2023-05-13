9610f20c56ed26779993e69b7d23a6dc6cd17dcc
Oppadnasser <oppadnasser@gmail.com>
2023-05-13 21:40:42 +0300

    payment method

--- Payment.java
+++ Payment.java
@@ -1,3 +1,5 @@
+// This class is responsible for payment with cash process
+
 public class Payment {
     public void payWithCash(double amount, Address address) {
         System.out.println("Delivery Will Go To Apartment No. " + address.getApartmentNumber() + " - Home No. " + address.getHomeNumber() + " - In " + address.getStreet() + " Street.");
